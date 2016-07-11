f28daba8ea979fec8072192a5b79bdebf76fe845
AntonKorobkov <korobgit@gmail.com>
2016-07-11 19:31:04 +0300

    added a method

--- api_test.py
+++ api_test.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import requests
+import pandas as pd
 
 res = requests.get('http://openapi.clearspending.ru/restapi/v3/contracts/select/?customerregion=48&okpd=92.40.10.111')
 if res.ok:
@@ -9,21 +10,35 @@ if res.ok:
 
 class RequestHandler:
     """
-    передавать экземпляру этого класса специальным
+    Передавать экземпляру этого класса специальным
     образом сформированные строки
+    предполагаем что номер региона всегда дан
     """
 
-    request_string = 'http://openapi.clearspending.ru/restapi/v3/contracts/select/?'
+    request_string = 'http://openapi.clearspending.ru/restapi/v3/contracts/select/?customerregion='
 
-    def send_request(self, regnum, *kwargs):
+    def __init__(self):
+        self.response = {}
+
+    def send_request(self, regnum, **kwargs):
         """
         отправить запрос к апи, получить
         json
-        предполагаем что один именнованый аргумент
-        который либо okdp либо okpd
+        предполагаем что именованный аргумент только один
+        либо okdp либо okpd
         :param regnum:
         :param args:
         :return:
         """
+        classcode = list(kwargs.keys())[0]
+        req = ''.join([RequestHandler.request_string, regnum, '&', classcode, '=', kwargs[classcode]])
+        res = requests.get(req)
+
+        return res.json()
+
+    def run_everything(self):
+        pass
 
 
+my_handler = RequestHandler()
+test_result = my_handler.send_request('48', okpd='92.40.10.111')
